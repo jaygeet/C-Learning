4f90401cf1864dd6e29a751d9ec72643066b0fbc
JT <jaytein>
2017-08-08 02:00:16 +0530

    Python file

---
+++ P.py
@@ -0,0 +1,48 @@
+#~/Desktop $ python3 ./P.py     ; Checkout C.c for C equivalent 
+#C-->P
+
+1) Pending while loop uncomment it
+
+for i in range(1,10):
+    a=i+2
+    b=i-2
+    print(b,i,a)
+
+
+
+
+print("Hello, Goody Morning"); # /* Print comment */
+print("F       C")    ;
+
+
+for f in range(0,300,20): #for loop
+    a=(5*(f-32))/9
+    print(f,"%.1f" % a);
+ 
+'''   
+    f=0;
+   
+   while (f<=upper)                /* While Loop*/
+
+   {
+        c=(5*(f-32))/9;
+        printf ("%5.0f %10.3f\n",f,c);
+        f=f+Step;
+    }
+ 
+
+  
+'''
+
+a=b=100 
+s=a+b ; # Sum */
+print(s)
+   
+z=10.2;
+x=z+s;    # Float*/
+print(x,z);
+z=a+x ; # Sum */
+print(z); 
+
+
+
